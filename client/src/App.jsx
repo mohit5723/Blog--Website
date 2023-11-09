cefc2118ead8001fe6cdfd0760290590ae86a6d3
Mohitpuri Goswami <mohitgoswami379@gmail.com>
2023-11-09 19:25:11 +0530

    completed html and css

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css"
+import Header from "./header"
+import Post from "./post"
+
+function App() {
+
+  return (
+    
+   
+      <main>
+       <Header /> 
+       <Post />
+       <Post />
+       <Post />
+
+      </main>
+  
+  )
+}
+
+export default App
