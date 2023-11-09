286e0617d55fab161ed6b657f720180ea7758321
Mohitpuri Goswami <mohitgoswami379@gmail.com>
2023-11-09 21:46:43 +0530

    added routers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,30 @@
 import "./App.css"
 import Header from "./header"
 import Post from "./post"
+import {Routes,Route} from "react-router-dom"
+import Layout from "./layout"
+import IndexPage from "./pages/indexpage"
+import LoginPage from "./pages/loginpage"
+import RegisterPage from "./pages/registerpage"
 
 function App() {
 
   return (
-    
+    <Routes>
+      <Route path="/" element={<Layout />} >
+        <Route index element={
+        <IndexPage />
+        } />     
+        <Route path={'/login'} element={
+        <LoginPage />
+        } />
+        <Route path={'/register'} element={
+        <RegisterPage />
+        } />
+      </Route>
+    </Routes>
    
-      <main>
-       <Header /> 
-       <Post />
-       <Post />
-       <Post />
-
-      </main>
+      
   
   )
 }
